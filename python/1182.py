3cbc8b8107709facaae8120c9b742d70101bef6d
Thorji <ledo26611@gmail.com>
2023-03-08 14:38:56 -0300

    solved 118

---
+++ python/1182.py
@@ -0,0 +1,10 @@
+column = int(input())
+operation = input().lower()
+
+matrix = [[float(input()) for i in range(0, 12)] for j in range(0 , 12)]
+
+cur = [matrix[i][column] for i in range(0, 12)]
+
+sumAll = sum(cur)
+
+print("%.1f" % (sumAll if operation == "s" else (sumAll / 12)))
