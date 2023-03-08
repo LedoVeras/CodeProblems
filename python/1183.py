66dfac10a7e5ebd6bef0acb362986174ce18c51c
Thorji <ledo26611@gmail.com>
2023-03-08 14:59:04 -0300

    solved 1183

---
+++ python/1183.py
@@ -0,0 +1,15 @@
+operation = input().lower()
+
+matrix = [[float(input()) for i in range(0, 12)] for j in range(0 , 12)]
+
+
+cur = [matrix[i][(1 + i):] for i in range(0, 12)]
+
+newlist = []
+
+for x in cur:
+    newlist.extend(x)
+    
+sumAll = sum(newlist)
+
+print("%.1f" % (sumAll if operation == "s" else (sumAll / len(newlist))))
