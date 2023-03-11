c30bf183f660d99a816e22b7a6e5c8e98fa144ce
Thorji <ledo26611@gmail.com>
2023-03-11 02:09:31 -0300

    solved 1003

---
+++ Beecrowd/Java/1003-Soma Simples.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    private static final double pi = 3.14159f;
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("SOMA = " + sum);
+    }
+}
