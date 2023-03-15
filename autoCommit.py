a551ce4cbce09658de1caa4a778eb711c9a17dc8
Thorji <ledo26611@gmail.com>
2023-03-14 23:13:18 -0300

    Reformat Date

--- autoCommit.py
+++ autoCommit.py
@@ -12,32 +12,27 @@ for i in range(1):
 
     py.hotkey("ctrl", "n")
     py.hotkey("ctrl", "s")
+    
     time.sleep(1)
     py.hotkey("ctrl", "v")
     py.write(".js") 
-    py.moveTo(1500, 190)
-    py.doubleClick()
-    py.moveTo(1500, 142)
-    py.doubleClick()
+    py.doubleClick(1500, 190)
+    py.doubleClick(1500, 142)
     py.press("enter")
-    py.moveTo(2375, 1050)
-    py.click()
+    py.click(2375, 1050)
 
     py.hotkey("ctrl", "shift", "d")
-    py.moveTo(1542, 177)
-    py.click()
+    py.click(1542, 177)
     py.hotkey("ctrl", "v")
     py.hotkey("ctrl", "shift", "e")
     py.hotkey("ctrl", "tab")
     py.hotkey("ctrl", "tab")
 
-
     py.hotkey("alt", "tab")
     py.moveTo(70, 200)
     py.scroll(-10000)
 
-    py.moveTo(554, 775)
-    py.click()
+    py.click(554, 775)
     py.hotkey("ctrl", "a")
     py.hotkey("ctrl", "c")
     py.hotkey("ctrl", "w")
@@ -47,8 +42,7 @@ for i in range(1):
     py.hotkey("ctrl", "s")
 
     py.hotkey("ctrl", "shift", "d")
-    py.moveTo(1542, 177)
-    py.click()
+    py.click(1542, 177)
     py.hotkey("ctrl" , "enter")
     py.hotkey("ctrl", "shift", "e")
     py.hotkey("ctrl", "tab")
