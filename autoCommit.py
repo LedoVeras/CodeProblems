5ec39a40fb76cf8d6434cd48ac62d25d3bb5a463
Thorji <ledo26611@gmail.com>
2023-03-14 22:34:15 -0300

    420004250043000435004400044500450002468

--- autoCommit.py
+++ autoCommit.py
@@ -2,12 +2,15 @@ import pyautogui as py
 import pyperclip as clip
 import time
 
-py.PAUSE = 1
+py.PAUSE = 0.2
+
 
 py.moveTo(70, 200)
 py.drag(900, 0, 0.2, button='left')
 py.hotkey("ctrl", "c")
 py.hotkey("alt", "tab")
+
+
 py.hotkey("ctrl", "alt", "n")
 py.hotkey("ctrl", "v")
 py.write(".js") 
@@ -17,5 +20,19 @@ py.click()
 py.hotkey("ctrl", "v")
 py.hotkey("ctrl", "enter")
 
-time.sleep(2)
+
+py.hotkey("alt", "tab")
+py.moveTo(70, 200)
+py.scroll(-10000)
+
+py.moveTo(554, 775)
+py.click()
+py.hotkey("ctrl", "a")
+py.hotkey("ctrl", "c")
+py.hotkey("alt", "tab")
+
+
+
+
+time.sleep(4)
 print(py.position())
