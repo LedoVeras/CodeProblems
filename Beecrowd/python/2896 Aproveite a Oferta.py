9046ee2f121e5cd55a2b467282b6aa46707381f1
Thorji <ledo26611@gmail.com>
2023-03-15 21:58:23 -0300

    solved 2896 Aproveite a Oferta

--- Beecrowd/python/2896 Aproveite a Oferta.py
+++ Beecrowd/python/2896 Aproveite a Oferta.py
@@ -1,21 +1,5 @@
-f, s, g, u, d = [int(x) for x in input().split(" ")]
+T = int(input())
 
-i = 0
-
-while True:
-    prevS = s
-    
-    dir = (s - g)
-    if(dir < 0):
-        s += u
-    elif(dir > 0):
-        s += -d
-    else:
-        break
-    print(s, dir)
-    i += 1
-    
-    if(i > 1000 or prevS == s):
-        break
-    
-print(i)
+for i in range(T):
+    N, K = [int(x) for x in input().split(" ")]
+    print((N % K) + (N // K))
