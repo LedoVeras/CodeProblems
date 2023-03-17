5e9720ffccfbf2d68331abaefcf5ff9d7e7952e0
Thorji <ledo26611@gmail.com>
2023-03-17 01:35:26 -0300

    solved 3142 Excel Bugado

---
+++ Beecrowd/python/3142 Excel Bugado.py
@@ -0,0 +1,17 @@
+alphabet = [ "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",]
+
+while True:
+    try: 
+        col = input()
+    except EOFError as e:
+        break
+    
+    sz = len(col)
+    numb = 0
+    
+    for n in range(sz):
+        i = alphabet.index(col[n]) + 1
+        
+        numb += i * (26 ** (sz - n - 1))
+    
+    print(numb if numb <= 16384 else "Essa coluna nao existe Tobias!")
